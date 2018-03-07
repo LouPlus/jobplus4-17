bfb9356a84a7ed3848b197f5addb5bb4a5a59d1d
Wang Shawn <wangshawnxyz@outlook.com>
2018-03-08 01:35:55 +0800

    #1 base.html中添加了当前用户状态的判断 #2 添加了登录页面 #3 完成了基础的登录功能 #4 完成退出登录功能 #5 对登录功能进行完善：已经登录的用户不能再次登录 #6 在app.py文件中添加了register_extension和user_loader函数

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,14 +1,38 @@
-from flask import Blueprint, render_template
-from jobplus.models import db, Job
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session
+from jobplus.models import db, Job, User
 from jobplus.forms import *
+from flask_login import login_user, logout_user, login_required
+
 # 省略了 url_prefix，那么默认就是 '/'
+# 加上了url_prefix='/', 那么login的路由就变成 host//login了！！！ 两个'/'
 front = Blueprint('front', __name__)
 
 @front.route('/')
+@front.route('/index')
 def index():
+	# print(session)
 	jobs = Job.query.all()
 	return render_template('index.html', jobs=jobs)
 @front.route('/login', methods=['GET', 'POST'])
 def login():
+	if session.get('user_id'):
+		flash('您已登录', 'info')
+		return redirect(url_for('front.index'))
 	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user:
+			login_user(user, form.remember_me.data)
+			return redirect(url_for('front.index'))
+		else:
+			flash('账号与密码不匹配', 'danger')
 	return render_template('login.html', form=form)
+
+@front.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash("注销成功", "success")
+	# print("logout")
+	return redirect(url_for('front.index'))
+	return render_template(url_for('front.index'))
