596de4580b1d6ee23bff4963a166eace619c06a6
Wang Shawn <wangshawnxyz@outlook.com>
2018-03-09 00:13:37 +0800

    #1 添加了登录页面 > #2 添加了LoginForm类 > #3 添加了front.login路由 > #4 实现了基本的登录功能 > #5 实现了注销功能

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template
 from jobplus.config import configs
 from flask_migrate import Migrate
-from jobplus.models import db
+from jobplus.models import db, User
+from flask_login import LoginManager
 
 def register_blueprints(app):
 	from .handlers import front
 	app.register_blueprint(front)
+def register_extension(app):
+	#注册数据库
+	db.init_app(app)
+	Migrate(app, db)
+	#登录功能扩展
+	login_manager = LoginManager()
+	login_manager.init_app(app)
+
+	@login_manager.user_loader
+	def user_loader(id):
+		#登录扩展的回调函数，用来判断用户的登录状态
+		return User.query.get(id)
+	
+	login_manager.login_view = 'front.login'
+
 
 def create_app(config):
 	""" App 工厂"""
 	app = Flask(__name__)
 	app.config.from_object(configs.get(config))
-	db.init_app(app)
-	Migrate(app, db)
+
 	register_blueprints(app)
+	register_extension(app)
 	# print(app.url_map)
 	return app
 
--- jobplus/models.py
+++ jobplus/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import UserMixin
 db = SQLAlchemy()
 
 class Base(db.Model):
@@ -12,7 +12,7 @@ class Base(db.Model):
 						   default=datetime.utcnow,
 						   onupdate=datetime.utcnow)
 
-class User(Base):
+class User(Base, UserMixin):
 	__tablename__ = 'user'
 
 	ROLE_USER = 10     # 求职者
